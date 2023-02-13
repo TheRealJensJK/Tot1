df8f38367ae1ef517bec0568930d5451ac054730
TheRealJensJK <jensnic@stud.ntnu.no>
2023-02-13 14:53:55 +0100

    added comments for code readebility and uppdated the README.md

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # Tot1 Web page
-a work in proges site can be viewed here: https://therealjensjk.github.io/Tot1/
+A work in proges site can be viewed here: https://therealjensjk.github.io/Tot1/
+
+Feedback needed on:
+- Colour pallet
+- Header image
+- Should the **Tot1** text in the header have 
+
+
+---
+## How to read code
+
+In the index.html file comments are in the brackets like this:
+` <!-- --> `
+
+In the style.css file comments are in brackets like this: 
+` /*  */ `
+
+In script.js file comments behind // like this:
+` // `
--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 //setting the current date as the max date for the date input
 window.onload = setMaxDate();
 
-//get date from html element
-//find current date
-//calculate age in years and months
-//output age in years and months to html tag using innerHTML
+
+//calculate the age of the applicant based on the input in the form, the age is not displayed to the user, but is included in the email sent to the admin (to easier see the age, and not have to calculate age themself)
 function calculateAge() {
   var birthDate = document.getElementById('bday').value;
   var age = new Date().getFullYear() - new Date(birthDate).getFullYear();
@@ -20,7 +18,7 @@ function calculateAge() {
 }
 
 
-//set the max attribute of the date input to today's date
+//set the max attribute of the date input to today's date to prevent future dates as inputs
 function setMaxDate(){
     var today = new Date();
     var dd = today.getDate();
