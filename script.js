9deda599cf62c1e3918dc4c0cd074057c31ace14
TheRealJensJK <jensnic@stud.ntnu.no>
2023-02-16 01:03:16 +0100

    removed autoscroll, styled links, changed layout and added description of clan

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 //setting the current date as the max date for the date input
 window.onload = setMaxDate();
 
+
+
 //calculate the age of the applicant based on the input in the form, the age is not displayed to the user, but is included in the email sent to the admin (to easier see the age, and not have to calculate age themself)
 function calculateAge() {
   var birthDate = document.getElementById('birthday').value;
@@ -32,37 +34,3 @@ function setMaxDate(){
   today = yyyy+'-'+mm+'-'+dd;
   document.getElementById("birthday").setAttribute("max", today);
 }
-
-
-
-//preventing the scroll function from running on phones and tablets
-function isMobile() {
-  return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-}
-  
-if (!isMobile()) {
-  //disable scrolling on the page
-  function disableScroll(){
-    // Get the current page scroll position
-    scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-
-    window.onscroll = function() {
-      window.scrollTo(scrollLeft, scrollTop);
-    };
-  }
-
-  function enableScroll() {
-    window.onscroll = function() {};
-  }
-  
-
-  document.addEventListener("scroll", function firstScroll(){
-    window.scrollTo({behavior: 'smooth' , top: window.innerHeight});
-    
-    enableScroll();
-  },{once:true});
-}
-  
-  
-
